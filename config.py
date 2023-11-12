1fb19a5fc345023b5866f7bd16959558dac40a57
Jaime Pineda <jaime.e.pineda@gmail.com>
2023-11-12 17:50:15 +0100

    Update setups -add config file -add pickle files with results from Power Spectrum, this avoid reruning them everytime -add figures with corner plot from the EMCEE fit

---
+++ config.py
@@ -0,0 +1,31 @@
+import astropy.units as u
+
+# Config file keeps the filenames across the different python 
+# files and notebooks
+
+
+#
+# Integrated intensity maps
+#
+file_TdV_13co = 'data/NGC1333_13CO_1-0_TdV.fits'
+file_TdV_c18o = 'data/NGC1333_SE_C18O_TdV.fits'
+file_TdV_h13cop_pad = 'data/NGC1333_H13COp_L17-merged_fix_pad_TdV.fits'
+file_TdV_h13cop = 'data/NGC1333_H13COp_L17-merged_fix_TdV.fits'
+file_TdV_hnc_pad = 'data/NGC1333_HNC_L23-merged_fix_pad_TdV.fits'
+file_TdV_hnc = 'data/NGC1333_HNC_L23-merged_fix_TdV.fits'
+
+#
+# Pickle files with each power-spectrum
+#
+file_pickle_13co = 'data/powerspec_NGC1333_13CO_1-0_TdV.pickle'
+file_pickle_13co_apod = 'data/powerspec_NGC1333_13CO_1-0_TdV_apod.pickle'
+file_pickle_c18o = 'data/powerspec_NGC1333_SE_C18O_TdV.pickle'
+file_pickle_c18o_apod = 'data/powerspec_NGC1333_SE_C18O-TdV_apod.pickle'
+file_pickle_h13cop_pad = 'data/powerspec_NGC1333_H13COp_L17-merged_fix_pad_TdV.pickle'
+file_pickle_h13cop = 'data/powerspec_NGC1333_H13COp_L17-merged_fix_TdV.pickle'
+file_pickle_hnc_pad = 'data/powerspec_NGC1333_HNC_L23-merged_fix_pad_TdV.pickle'
+file_pickle_hnc = 'data/powerspec_NGC1333_HNC_L23-merged_fix_TdV.pickle'
+
+file_pickle_hcn = 'data/powerspec_NGC1333_HCN_L21-TdV-merged_fix_TdV.pickle'
+
+distance = 300. * u.pc
